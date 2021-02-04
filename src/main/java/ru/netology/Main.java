cfb99ae62877cd177aa8be1656872e606e5163a8
Rishat Suleymanov <spk_irbis@mail.ru>
2021-02-04 22:18:38 +0300

    Add files Ints.java and IntsCalculator.java

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -15,9 +15,9 @@ public class Main {
 //                        .result()
 //        );
 
-//        Ints intsCalc = new IntsCalculator();
-//        System.out.println(intsCalc.sum(2, 2));
-//        System.out.println(intsCalc.sum(10, 22));
-//        System.out.println(intsCalc.pow(2, 10));
+        Ints intsCalc = new IntsCalculator();
+        System.out.println(intsCalc.sum(2, 2));
+        System.out.println(intsCalc.sum(10, 22));
+        System.out.println(intsCalc.pow(2, 10));
     }
 }
