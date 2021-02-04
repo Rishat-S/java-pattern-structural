b06743504a74b2a58c15fc65a7507d0bb4569f12
Rishat Suleymanov <spk_irbis@mail.ru>
2021-02-04 21:57:49 +0300

    Add Calculator.java

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,10 +1,23 @@
 package ru.netology;
 
 /**
- * Hello world!
+ * Main program
  */
 public class Main {
     public static void main(String[] args) {
-        //TODO
+
+//        Calculator calc = new Calculator();
+//        System.out.println(
+//                calc.newFormula()
+//                        .addOperand(5)
+//                        .addOperand(15)
+//                        .calculate(Calculator.Operation.MULT)
+//                        .result()
+//        );
+
+//        Ints intsCalc = new IntsCalculator();
+//        System.out.println(intsCalc.sum(2, 2));
+//        System.out.println(intsCalc.sum(10, 22));
+//        System.out.println(intsCalc.pow(2, 10));
     }
 }
